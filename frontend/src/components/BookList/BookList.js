d7fa25f25010423bca9fe5816159dafaabc110c9
Daryna Utkina <dashaelbereth1@gmail.com>
2025-04-26 15:42:13 +0300

    added delete btn and its logic

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,8 +1,16 @@
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import styles from './BookList.module.css';
+import { deleteBook } from '../../redux/books/actionCreators';
 
 const BookList = () => {
   const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+
+  const handleDeleteBook = (id) => {
+    dispatch(deleteBook(id));
+  };
+
   return (
     <div className={`app-block ${styles['book-list']}`}>
       <h2>Book List</h2>
@@ -14,6 +22,11 @@ const BookList = () => {
             <li key={book.id}>
               <div className={`${styles['book-info']}`}>
                 {++index}.{book.title} by <strong>{book.author}</strong>
+              </div>
+              <div className={`${styles['book-actions']}`}>
+                <button onClick={() => handleDeleteBook(book.id)}>
+                  Delete
+                </button>
               </div>
             </li>
           ))}
--- frontend/src/redux/books/actionCreators.js
+++ frontend/src/redux/books/actionCreators.js
@@ -7,9 +7,10 @@ export const addBook = (newBook) => {
   };
 };
 
-export const deleteBook = () => {
+export const deleteBook = (id) => {
   return {
     type: a.DELETE_BOOK,
+    payload: id,
   };
 };
 
