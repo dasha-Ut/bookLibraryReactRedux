b37c3d3d5cc364cb86a607530c5fd82956aabe02
Daryna Utkina <dashaelbereth1@gmail.com>
2025-04-27 23:03:00 +0300

    added errorSlice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import filterReducer from './slices/filterSlice';
 import booksReducer from './slices/booksSlice';
+import errorReducer from './slices/errorSlice';
 
 const store = configureStore({
   reducer: {
     books: booksReducer,
     filter: filterReducer,
+    error: errorReducer,
   },
   devTools: process.env.NODE_ENV !== 'production', // Включити DevTools тільки в режимі розробки
 });
