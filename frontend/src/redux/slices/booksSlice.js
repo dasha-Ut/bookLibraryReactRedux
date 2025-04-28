f3b220bf1b029665b93cdb9c70e3edf0ab766e25
Daryna Utkina <dashaelbereth1@gmail.com>
2025-04-28 09:48:27 +0300

    added another option for extraReducers

--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -36,11 +36,19 @@ const booksSlice = createSlice({
       });
     },
   },
-  extraReducers: (builder) => {
-    builder.addCase(fetchBook.fulfilled, (state, action) => {
+  // option 1
+  // extraReducers: (builder) => {
+  //   builder.addCase(fetchBook.fulfilled, (state, action) => {
+  //     if (action?.payload?.title && action?.payload?.author)
+  //       state.push(createBookWithId(action.payload, 'API'));
+  //   });
+  // },
+  // option 2
+  extraReducers: {
+    [fetchBook.fulfilled]: (state, action) => {
       if (action?.payload?.title && action?.payload?.author)
         state.push(createBookWithId(action.payload, 'API'));
-    });
+    },
   },
 });
 
